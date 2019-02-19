6962f907c9b099fc0b1d7ca64f47ecc1fb645143
abcde090 <229243386@qq.com>
2019-02-19 16:25:27 +1000

    Use redux to change states

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles/main.css';
 
+import { Provider } from "react-redux";
+import store from "./store";
 import Header from './weather/Header';
 import Footer from './weather/Footer';
 import WeatherChannel from './weather/WeatherChannel';
+import WeatherChannelRedux from './weather/WeatherChannelRedux';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="weather-channel__container">
         <Header />
-        <WeatherChannel />
+        <WeatherChannelRedux />
         <Footer />
       </div>
+      </Provider>
     );
   }
 }
