44ad0d0c8012870e9831ba53ccd953df02704650
abcde090 <229243386@qq.com>
2019-02-28 02:14:00 +1000

    Updates

---
+++ precache-manifest.aad057b581c8268a22507eda9e17ea64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "6bd2e4fbab4df3b64a7b",
+    "url": "/react-app/static/js/main.6bd2e4fb.chunk.js"
+  },
+  {
+    "revision": "8d61fe35d337f2986d35",
+    "url": "/react-app/static/js/1.8d61fe35.chunk.js"
+  },
+  {
+    "revision": "6bd2e4fbab4df3b64a7b",
+    "url": "/react-app/static/css/main.8f02931b.chunk.css"
+  },
+  {
+    "revision": "8b4d3e8b264d2f7299a5a6e40babbeb1",
+    "url": "/react-app/index.html"
+  }
+];
